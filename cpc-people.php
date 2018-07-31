8fa41d0a688904070fc98162c0a952e0a37bcad6
Tony Bird <tonybird@live.unc.edu>
2018-07-31 15:08:00 -0400

    Initial commit

---
+++ cpc-people.php
@@ -0,0 +1,28 @@
+<?php
+   /*
+   Plugin Name: CPC People
+   Description: Display CPC Mugboard, Faces of CPC and Greetings and Farewells widgets
+   Version: 1.1
+
+   Author: Tony Bird
+   Author URI: http://antoniobird.com
+
+   License: GNU General Public License v2 or later
+   License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+   */
+
+   include( plugin_dir_path( __FILE__ ) . 'options-menu.php');
+   $opts = get_option( 'cpc_people_options' );
+   $projectsdb = new wpdb($opts['username'], $opts['password'], $opts['table'], $opts['hostname']);
+
+   include( plugin_dir_path( __FILE__ ) . 'widgets/faces-widget.php');
+   include( plugin_dir_path( __FILE__ ) . 'widgets/greetings-widget.php');
+   include( plugin_dir_path( __FILE__ ) . 'widgets/project-widget.php');
+
+   include( plugin_dir_path( __FILE__ ) . 'shortcodes/mugboard.php');
+   //Datatables demo shortcodes
+   include( plugin_dir_path( __FILE__ ) . 'shortcodes/mugboard-dt.php');
+   include( plugin_dir_path( __FILE__ ) . 'shortcodes/searchable-dt.php');
+
+?>
---
+++ widgets/project-widget.php
@@ -0,0 +1,59 @@
+<?php
+function project_load_widget() {
+    register_widget( 'project_widget' );
+}
+add_action( 'widgets_init', 'project_load_widget' );
+
+class project_widget extends WP_Widget {
+
+function __construct() {
+parent::__construct('project_widget', __('CPC Projects', 'project_widget_domain'),
+array( 'description' => __( 'Displays random CPC project', 'project_widget_domain' ), )
+);
+}
+
+// front-end
+public function widget( $args, $instance ) {
+$title = apply_filters( 'widget_title', $instance['title'] );
+
+echo $args['before_widget'];
+if ( ! empty( $title ) )
+echo $args['before_title'] . $title . $args['after_title'];
+
+global $projectsdb;
+$randproj = $projectsdb->get_results( "SELECT title, pi, url, image FROM projects ORDER BY RAND() LIMIT 1" );
+
+foreach ($randproj as $proj) {
+  echo "<img src='http://devweb11.cpc.unc.edu/images/$proj->image' width='100px' class='alignleft' style='margin-right: 10px;'>";
+  echo "<b><a href='$proj->url'>$proj->title</a></b>";
+  echo "<p><i>$proj->pi</i></p>";
+}
+
+echo $args['after_widget'];
+}
+
+// Backend
+public function form( $instance ) {
+if ( isset( $instance[ 'title' ] ) ) {
+$title = $instance[ 'title' ];
+}
+else {
+$title = __( 'project of CPC', 'project_widget_domain' );
+}
+
+// admin form
+?>
+<p>
+<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<?php
+}
+
+// Updating widget replacing old instances with new
+public function update( $new_instance, $old_instance ) {
+$instance = array();
+$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+return $instance;
+}
+}
